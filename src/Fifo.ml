4e464589852197f5ca9c216f12954ddc1a60441d
Sara KHICHANE <sara.khichane@etu.u-paris.fr>
2022-12-04 02:14:11 +0100

    fifo avec paire de listes

--- src/Fifo.ml
+++ src/Fifo.ml
@@ -8,9 +8,28 @@
 
 *)
 
-type 'a t = 'a list (* head of list = first out *)
+let append left right =
+  let rec aux acc l =
+    match l with
+    | [] -> acc
+    | x::r -> aux (x::acc) r
+  in aux right (List.rev left) ;;
+
+(* first list is the front of the queue, second list is the back of the queue *)
+type 'a t = 'a list * 'a list
+let empty = ([], [])
+let push x (front, back) = (front, x :: back)
+let pop (front, back) = match front with
+  | [] -> (match List.rev back with
+      | [] -> failwith "empty queue"
+      | x :: reste -> (reste, [])) (*si la première liste est vide, on reverse la deuxieme et on la met dans la première*)
+  | x :: reste -> (reste, back)
+let of_list l = (l, [])
+let to_list (front, back) = append front (List.rev back)
+
+(* type 'a t = 'a list (* head of list = first out *)
 let empty = []
 let push x q = q@[x]
 let pop q = match q with x::q' -> x, q' | [] -> raise Not_found
 let of_list l = List.rev l
-let to_list l = l
+let to_list l = l *)
