f3a4b4688acb887c7132f4c55df9cf85e2f980a1
Sara KHICHANE <sara.khichane@etu.u-paris.fr>
2022-12-07 11:13:28 +0100

    CORRECTION DE L'IMPLÉMENTATION DE LA FIFO

--- src/Fifo.ml
+++ src/Fifo.ml
@@ -8,7 +8,7 @@
 
 *)
 
-(* let append left right =
+let append left right =
   let rec aux acc l =
     match l with
     | [] -> acc
@@ -18,18 +18,23 @@
 (* first list is the front of the queue, second list is the back of the queue *)
 type 'a t = 'a list * 'a list
 let empty = ([], [])
-let push x (front, back) = (front, x :: back)
-let pop (front, back) = match front with
-  | [] -> (match List.rev back with
-      | [] -> failwith "empty queue"
-      | x :: reste -> (reste, [])) (*si la première liste est vide, on reverse la deuxieme et on la met dans la première*)
-  | x :: reste -> (reste, back)
+let push x q = 
+  match q with
+  | (front, back) -> (front, x :: back)
+let pop q = 
+  match q with
+  | (front, back) -> 
+    match front with
+    | [] -> (match List.rev back with
+        | [] -> failwith "empty queue"
+        | x :: reste -> (x, (reste, []))) (*si la première liste est vide, on reverse la deuxieme et on la met dans la première*)
+    | x :: reste -> (x, (reste, back))
 let of_list l = (l, [])
-let to_list (front, back) = append front (List.rev back) *)
+let to_list (front, back) = append front (List.rev back)
 
-type 'a t = 'a list (* head of list = first out *)
+(* type 'a t = 'a list (* head of list = first out *)
 let empty = []
 let push x q = q@[x]
 let pop q = match q with x::q' -> x, q' | [] -> raise Not_found
 let of_list l = List.rev l
-let to_list l = l
+let to_list l = l *)
